91e7b378627abb8f614fd1853fe2eaac2029b923
mohammad <mohammadhp858@gmail.com>
2024-12-17 21:29:48 +0330

    add custom user model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.core.validators import RegexValidator
+from .managers import CustomUserManager
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    phone = models.CharField(
+        max_length=11,
+        unique=True,
+        validators=[
+            RegexValidator(regex=r"^\d{11}", message="شماره تلفن نامعتبر است.")
+        ],
+    )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+    USERNAME_FIELD = "phone"
+    REQUIRED_FIELDS = []
