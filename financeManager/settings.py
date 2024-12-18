759bc4b110b4974ea237feaf6c23baa1b4a7947b
mohammad <mohammadhp858@gmail.com>
2024-12-18 13:16:35 +0330

    add authentication class

--- financeManager/settings.py
+++ financeManager/settings.py
@@ -132,5 +132,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 
 AUTH_USER_MODEL = 'account.CustomUser'
