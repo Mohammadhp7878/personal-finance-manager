a0f8490842e6674c9839419b1198e75cae3921dd
mohammad <mohammadhp858@gmail.com>
2024-12-19 11:03:17 +0330

    add url to verify otp

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SendOtp
+from .views import SendOtp, VerifyOtp
 
 
 urlpatterns = [
         path('send_otp/', SendOtp.as_view(), name='send_otp'),
+        path('verify_otp/', VerifyOtp.as_view(), name='verify_otp'),
 ]
