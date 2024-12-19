e56e96dcb9cc89b16fec1663043c0603704eba77
mohammad <mohammadhp858@gmail.com>
2024-12-19 11:22:40 +0330

    add BaseModel

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django_jalali.db import models as jmodels
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at = jmodels.jDateTimeField(auto_now_add=True)
+    updated_at = jmodels.jDateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+    
