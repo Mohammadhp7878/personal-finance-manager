7f375e5a44dde8023c455365c7f433a03ab165ef
mohammad <mohammadhp858@gmail.com>
2024-12-24 20:50:25 +0330

    add model for crypto assets

--- finance/models.py
+++ finance/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from core.models import BaseModel
 
 
-class CryptoAsset(models.Model):
-    ...
+class CryptoAsset(BaseModel):
+    id = models.IntegerField(primary_key=True, unique=True, blank=True)
+    name = models.CharField(max_length=30)
+    symbol = models.CharField(max_length=10, null=True, blank=True)
+    balance = models.DecimalField(decimal_places=4, max_digits=10)
+    price_at_purchase = models.IntegerField()   
+     
