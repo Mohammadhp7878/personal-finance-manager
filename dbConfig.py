759a6d0dbe1d6c7148e69ddb0ae8570351ddd45d
mohammad <mohammadhp858@gmail.com>
2024-12-28 20:29:04 +0330

    remove extra file

--- dbConfig.py
+++
@@ -1,9 +0,0 @@
-from decouple import config
-
-db_config = {
-    "dbname" : config("dbname"),
-    "user" : config("user"),
-    "password" : config("password"),
-    "host" : config("host"),
-    "port" : config("port"),
-}
