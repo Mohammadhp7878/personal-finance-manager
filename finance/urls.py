11175eb94735e6102b381bc1f01c18c6c3fe14f2
mohammad <mohammadhp858@gmail.com>
2025-01-02 14:42:55 +0330

    change names

--- finance/urls.py
+++ finance/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import AddCryptoAsset
+from .views import CryptoAssetView
 
 
 urlpatterns = [
-    path('add_crypto/', AddCryptoAsset.as_view(), name="add_crypto"),
+    path('add_crypto/', CryptoAssetView.as_view(), name="add_crypto"),
 ]
