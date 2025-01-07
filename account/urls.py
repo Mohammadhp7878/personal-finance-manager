71b4b1c0991115f02ca8b5d7c4ceb103e1db6c1c
mohammad <mohammadhp858@gmail.com>
2025-01-07 23:13:17 +0330

    add urls for userprofile

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import SendOtp, VerifyOtp
+from .views import SendOtp, VerifyOtp, UserProfileView, UserListView
 
 
 urlpatterns = [
         path('send_otp/', SendOtp.as_view(), name='send_otp'),
         path('verify_otp/', VerifyOtp.as_view(), name='verify_otp'),
+        path('user_list/', UserListView.as_view(), name="user_list"),
+        path('user_profile/<int:pk>', UserProfileView.as_view(), name='user_profile'),
 ]
