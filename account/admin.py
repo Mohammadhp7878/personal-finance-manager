5ea2a2c3c292903b64df78a4a6d9e1bff0340591
mohammad <mohammadhp858@gmail.com>
2025-01-17 15:53:58 +0330

    add models to admin panel

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CustomUser, UserProfile
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    ...
+    
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    ...
