004a3afbe182ea3865193f9e9157f504559319b3
mohammad <mohammadhp858@gmail.com>
2025-01-17 23:12:22 +0330

    add crypto to admin panel, add front folder to gitignore

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CryptoAsset
 
-# Register your models here.
+
+@admin.register(CryptoAsset)
+class CryptoAssetAdmin(admin.ModelAdmin):
+    list_display = ["user__phone", "name", "symbol", "balance"]
