7c0cc4042d1c77d52061bcb0f57c9d382eb86e8b
mohammad <mohammadhp858@gmail.com>
2025-01-17 18:25:10 +0330

    modify admin panel fields

--- account/admin.py
+++ account/admin.py
@@ -4,9 +4,19 @@ from .models import CustomUser, UserProfile
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    ...
+    list_display = ["id", "phone"]
+    list_per_page = 10
     
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    ...
+    list_display = ["user__id", "first_name", "last_name", "user__phone", "is_profile_completed"]
+    list_per_page = 10
+    # list_editable
+    
+    def is_profile_completed(self, obj):
+        if obj.first_name and obj.last_name and obj.email:
+            return "True"
+        else: 
+            return "False"
+         
